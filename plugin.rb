5e68f243aa6032fa23b7d8edaeca09b205cc22c4
Jaswinder <jaswindersinghsn97+1@gmail.com>
2017-02-27 07:39:42 +0530

    first commit

---
+++ plugin.rb
@@ -0,0 +1,16 @@
+# name: vip-section
+# about: This will hold secret key for the vip section
+# version: 0.1
+# authors: Ajet
+# url: To be added
+
+enabled_site_setting :vip_secret
+
+# Defining a route for the users to come to the route and being added to the VIP group
+
+add_admin_route 'vip_section.title', 'vip_section'
+add_admin_route 'plugins.title', 'plugins'
+
+Discourse::Application.routes.append do
+  get '/register/vip_section' => 'vip_section#index'
+end
